1ca26ad3a542c48b01953e7f6e1e8927a208f105
idilzanmohamuod-oss <idilzanmohamuod@gmail.com>
2026-05-05 02:00:26 +0300

    added exercise 5 folder

---
+++ JS Ex5/Ex5.js
@@ -0,0 +1,12 @@
+//  Arrow Function //
+console.log(" Arrow Function ");
+
+const Addition= (a,b) =>{
+   
+    console.log(a + b )
+}
+
+Addition(10,3);
+Addition(103,2);
+Addition(13,27);
+Addition(1000,3);
